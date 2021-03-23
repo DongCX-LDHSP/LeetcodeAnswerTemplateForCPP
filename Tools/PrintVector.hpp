9d96c1c1608160c0628c3d3a8c3e24370f0d804f
DongCX-LDHSP <52726994+DongCX-LDHSP@users.noreply.github.com>
2021-03-23 20:47:56 +0800

    修复&重构：不能输出空vector的bug

--- Tools/PrintVector.hpp
+++ Tools/PrintVector.hpp
@@ -11,24 +11,24 @@
 using namespace std;
 
 
-template <typename Content> void PrintVector(ostream& output, vector<Content> param) {
-    auto iter = param.begin();
-    auto end = param.end();
-    output << "{";
-    while(iter < end - 1) {
-        output << *iter;
-        output << ", ";
-        iter += 1;
-    }
-    output << *iter;
-    output << "}";
-}
-
 /**
  * 为vector重载<<运算符
  */
-template <typename Content> ostream &operator<<(ostream& output, const vector<Content> param) {
-    PrintVector(output, param);
+template <typename Content>
+ostream& operator<<(ostream& output, const vector<Content> param) {
+    output << "{";
+    if (param.size() > 0) {
+        auto iter = param.begin();
+        auto end = param.end();
+        while(iter < end - 1) {
+            output << *iter;
+            output << ", ";
+            iter += 1;
+        }
+        output << *iter;
+    }
+    output << "}";
+    return output;
 }
 
 

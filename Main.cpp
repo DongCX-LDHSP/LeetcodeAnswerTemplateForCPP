3b3bb00549cb0cd464543d451c6dff7bfc8b3560
DongCX-LDHSP <52726994+DongCX-LDHSP@users.noreply.github.com>
2021-03-23 18:56:53 +0800

    修改：使之适应多组数据测试的情景

--- Main.cpp
+++ Main.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
-#include "Solution.hpp"
 #include "Tools/PrintVector.hpp"
 #include "Tools/PrintPair.hpp"
 #include "Tools/ListNode.hpp"
 
 
 // include here
+#include "Solution.hpp"
+#include <vector>
 
 
 using namespace std;
@@ -13,12 +14,37 @@ using namespace std;
 
 int main() {
     // Input code here
-    // auto input;
+    // redefine input and answer here
+    typedef inputDataType input;
+    typedef answerDataType answer;
+    typedef pair<input, answer> UseCase;
+    vector<UseCase> inputAndAnswer;
+
+    // add use case here
+    inputAndAnswer.push_back();
 
     {
         Solution solution;
-        // run solution here;
-        auto result = solution;
+        bool allPassed = true;
+        cout << "Solution: " << endl;
+        for (auto useCase : inputAndAnswer) {
+            cout << useCase;
+            cout << ": ";
+
+            // call solution function here;
+            auto result = solution.function();
+            
+            cout << result;
+            if (result != useCase.second) {
+                allPassed = false;
+                cout << "    Fail";
+            }
+            else {
+                cout << "    Pass";
+            }
+            cout << endl;
+        }
+        cout << (allPassed == true ? "All passed!" : "Some case failed!") << endl;
     }
 
     cout << endl << "Process returned 0." << endl;
